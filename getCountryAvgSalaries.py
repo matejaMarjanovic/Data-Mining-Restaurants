0a2fa1083f00ee66e2fa03ae829dfed252e3582e
Mateja Marjanovic <mi15712@alas.matf.bg.ac.rs>
2018-09-10 22:23:02 +0200

    Instead of country code now in the main csv file is the name of the country and a file which has salaries of all world countries has been created using web scraping.

---
+++ getCountryAvgSalaries.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from bs4 import BeautifulSoup as soup
+from urllib.request import urlopen as uReq
+
+salariesUrl = "http://www.nationmaster.com/country-info/stats/Cost-of-living/Average-monthly-disposable-salary/After-tax"
+
+uClient = uReq(salariesUrl)
+pageHtml = uClient.read()
+uClient.close()
+
+pageSoup = soup(pageHtml, "html.parser")
+allRows = pageSoup.table.findAll("tr")[1:]
+
+df = pd.DataFrame(columns = ["Country", "Average Salary"])
+
+for row in allRows:
+    country = row.a.span.text
+    avgSalary = float(row.findAll("td", {"class":"amount"})[0].text.strip()[1:].replace(",", ""))
+    df = df.append({"Country" : country, "Average Salary" : avgSalary}, ignore_index = True)
+    
+with open("countrySalaries.csv", "w") as csvFile:
+    csv = df.to_csv(index = True)
+    csvFile.write(csv)
