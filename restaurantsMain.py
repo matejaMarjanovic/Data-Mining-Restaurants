c6bf05b70193243457b0d77b2f551d6675a6fc7c
Mateja Marjanovic <mi15712@alas.matf.bg.ac.rs>
2018-09-13 13:44:50 +0200

    There were a few bugs with the data, so I fixed it by brute force. Avg price for two in euros columns has been added and now restaurants prices can be compared.

---
+++ restaurantsMain.py
@@ -0,0 +1,10 @@
+import pandas as pd
+print("***********", "*zomatoCountryAdded.csv*", "***********", "\n", sep="\n")
+
+df_restaurants = pd.read_csv("zomatoMissingValuesRemoved.csv")
+print(df_restaurants.head(), "\n")
+print(df_restaurants.count(), "\n")
+print(df_restaurants.describe(), "\n")
+for column in df_restaurants.columns:
+    print("Count values in column " + column, "\n")
+    print(df_restaurants[column].value_counts(dropna=False))
